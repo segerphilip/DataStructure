932f3f2b47bdba6bca90e4143c777ddc02510021
Philip Seger <seger.philip@gmail.com>
2016-01-29 09:19:22 -0500

    working on in lab assignment

---
+++ Lab1/src/Intro.java
@@ -0,0 +1,18 @@
+/**
+ * Created by philip on 1/29/16.
+ */
+import java.util.Scanner;
+
+public class Intro {
+    public static void main(String[] args) {
+//        declaration statement "Scanner scan", assign new Scanner object to scan, Scanner() call to the constructor
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = scan.nextLine();
+        System.out.println("What is your favorite color? ");
+        String color = scan.nextLine();
+        System.out.println("What is the speed of an unladen swallow? ");
+        String speed = scan.nextLine();
+        System.out.println(name + ", " + color + ", " + speed);
+    }
+}
