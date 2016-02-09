4a54ff81519215219c23653ba0699d04468ff8a1
Philip Seger <seger.philip@gmail.com>
2016-02-09 02:08:16 -0500

    Point class complete, now starting part 2 of assignment

--- assignment2_pseger/src/Point.java
+++ assignment2_pseger/src/Point.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 /**
  * Created by philip on 2/5/16.
  */
 public class Point {
     private double x;
     private double y;
+    private static final double TOLERANCE = .01;
 
     public Point(double x, double y) {
         this.x = x;
@@ -26,15 +29,43 @@ public class Point {
         y = val;
     }
 
-//    public void findDistance(Point ) {
-//
-//    }
+    private double findDistance(Point p) {
+        double distance = Math.sqrt(Math.pow(Math.abs(p.getX() - x), 2) + Math.pow(Math.abs(p.getY() - y), 2));
+        return distance;
+    }
+
+    private static boolean areEquidistant(Point p1, Point p2) {
+        if (p1.findDistance(p2) <= TOLERANCE) {
+            return true;
+        }
+        return false;
+    }
 
     public String toString() {
-        return "";
+        return "X: " + x + "\tY: " + y;
+    }
+
+    private static Point readPoint() {
+        Scanner scan =  new Scanner(System.in);
+        System.out.print("X:");
+        double x1 = scan.nextDouble();
+        System.out.print("Y:");
+        double y1 = scan.nextDouble();
+        Point pnts = new Point(x1, y1);
+        return pnts;
     }
 
     public static void main(String[] args) {
-        Point pnt = new Point(15.5, 13.2);
+        System.out.println("Point 1");
+        Point pnt1 = readPoint();
+        System.out.println("Point 2");
+        Point pnt2 = readPoint();
+        System.out.println("Point 3");
+        Point pnt3 = readPoint();
+
+        if (areEquidistant(pnt1, pnt2) || areEquidistant(pnt2, pnt3) || areEquidistant(pnt1, pnt3))
+            System.out.println("Yes, there is equadistance");
+        else
+            System.out.println("Nope");
     }
 }
