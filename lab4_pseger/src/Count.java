41e6ab8f98eb7822669be8e9cba6edba1dd755cb
Philip  Seger <pseger@sci-e101-05-b.local>
2016-02-19 09:42:46 -0500

    during lab work on students

---
+++ lab4_pseger/src/Count.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+/**
+ * Created by philip and eliza on 2/18/16.
+ */
+public class Count {
+    private String word;
+    private int[] counts;
+    
+    public Count(String word) {
+        this.word = word.toUpperCase();
+        this.counts = new int[26];
+    }
+    
+    public void countArray() {
+        for (int i = 0; i < word.length(); i++) {
+            try {
+                int index = (int)(word.charAt(i) - 65);
+                counts[index] += 1;
+            }
+            catch (ArrayIndexOutOfBoundsException exception) {
+                System.out.println("Error: " + (char)(word.charAt(i)) + " is not allowed.");
+            }
+        }
+    }
+    
+    public String toString() {
+        String printString = "";
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] != 0) {
+                printString += ((char)(i + 65) + ": " + counts[i] + "\t"); 
+            }
+        }
+        return printString;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a word");
+        String word = scan.nextLine();
+        
+        Count c1 = new Count(word);
+        c1.countArray();
+        System.out.println(c1);
+    }
+}
--- lab4_pseger/src/Student.java
+++ lab4_pseger/src/Student.java
@@ -1,46 +1,50 @@
 /**
  * Created by philip on 2/18/16.
  */
-public class Student {
-    private String name;
-    private int hours;
-    private double quality;
-    private double GPA;
-
-    public Student(String name, int hours, double quality) {
-        this.name = name;
-        this.hours = hours;
-        this.quality = quality;
-        GPA = 0;
+public class Student implements Comparable<Student> {
+  private String name;
+  private int hours;
+  private double quality;
+  private double GPA;
+  
+  public Student(String name, int hours, double quality) {
+    this.name = name;
+    this.hours = hours;
+    this.quality = quality;
+    GPA = quality / hours;
+  }
+  
+  public int compareTo(Student student) {
+    return this.name.compareTo(student.name);
+  }
+  
+  private boolean isOnWarning() {
+    if ((GPA < 1.5 && hours < 30) || (GPA < 1.75 && hours < 1.75) || (GPA < 2.0 && hours >= 1.75)) {
+      return true;
     }
-
-    private void computeGPA() {
-        GPA = quality / hours;
-        if (GPA > 4.0) {
-            System.out.println("That's not possible!");
-        }
-        else if ((GPA < 1.5 && hours < 30) || (GPA < 1.75 && hours < 1.75) || (GPA < 2.0 && hours >= 1.75)) {
-            System.out.println("You're GPA is dangerously low!");
-        }
+    else {
+      return false;
     }
-
-    public String toString() {
-        return name + " has a GPA of " + GPA;
-    }
-
-    public static void main(String[] args) {
-        Student s1 = new Student("Pip", 50, 230.1);
-        Student s2 = new Student("Kelly", 84, 292.4);
-        Student s3 = new Student("Jonathan", 75, 200.1);
-        Student s4 = new Student("Bob", 10, 15);
-        s1.computeGPA();
-        System.out.println(s1);
-        s2.computeGPA();
-        System.out.println(s2);
-        s3.computeGPA();
-        System.out.println(s3);
-        s4.computeGPA();
-        System.out.println(s4);
-    }
+  }
+  
+  public String toString() {
+    return name + " has a GPA of " + GPA;
+  }
+  
+  public static void main(String[] args) {
+    Student s1 = new Student("Pip", 50, 230.1);
+    Student s2 = new Student("Kelly", 84, 292.4);
+    Student s3 = new Student("Jonathan", 75, 200.1);
+    Student s4 = new Student("Bob", 10, 15);
+    s1.isOnWarning();
+    System.out.println(s1);
+    s2.isOnWarning();
+    System.out.println(s2);
+    s3.isOnWarning();
+    System.out.println(s3);
+    s4.isOnWarning();
+    System.out.println(s4);
+    System.out.println("Is " + s4.name + " on warning? : " + s4.isOnWarning());
+  }
 }
 
