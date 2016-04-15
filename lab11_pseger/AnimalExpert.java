406cab732323fd6aef42e8bb44fcb3cb38f85818
Philip  Seger <seger.philip@gmail.com>
2016-04-15 10:20:47 -0400

    lab 11 work

---
+++ lab11_pseger/AnimalExpert.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;  // for reading from keyboard
+import javafoundations.*; // for the LinkedBinaryTree and all
+
+public class AnimalExpert {
+  
+  private LinkedBinaryTree<String> theTree;
+  
+  // sets up 'expert' database
+  public AnimalExpert(){
+    String e1 = "Is the animal big?";
+    String e2 = "Does it live in a cage?";
+    String e3 = "Does it eat grass?";
+    String e4 = "Does it meow?";
+    String e5 = "Does it chirp?";
+    //String e6 = "Does it have a long neck?";
+    String e6 = "Does it like water?";
+    String e7 = "Does it moo?";
+    String e8 = "It's a dog!";
+    String e9 = "It's a cat!";
+    String e10 = "It's a hamster!";
+    String e11 = "It's a bird!";
+    String e12 = "It's a lion!";
+    String e13 = "It's a crocodile!";
+    String e14 = "It's a horse!";
+    String e15 = "It's a cow!";
+    
+    //create the LinkedBinarTree
+
+  }
+
+  //define any other methods
+  
+  //add a main()
+
+} // AnimalExpert class
+
---
+++ lab11_pseger/Test.java
@@ -0,0 +1,31 @@
+// Testing iterators using BTNode
+// Sohie CS230 Lab 10
+
+import javafoundations.*;
+
+public class Test {
+  public static void main(String [] args) {
+    BTNode<String> s1 = new BTNode<String>("meckila");
+    BTNode<String> s2 = new BTNode<String>("meryl");
+    BTNode<String> s3 = new BTNode<String>("julia");  
+    BTNode<String> s4 = new BTNode<String>("rachel");
+    BTNode<String> s5 = new BTNode<String>("amal"); 
+    BTNode<String> s6 = new BTNode<String>("sharon");
+    BTNode<String> s7 = new BTNode<String>("farren"); 
+    BTNode<String> s8 = new BTNode<String>("sian");
+    BTNode<String> s9 = new BTNode<String>("kim");
+    BTNode<String> s10 = new BTNode<String>("cece");
+    BTNode<String> s11 = new BTNode<String>("wendy");
+    BTNode<String> s12 = new BTNode<String>("emily");
+    BTNode<String> s13 = new BTNode<String>("stephanie");
+    BTNode<String> s14 = new BTNode<String>("aline");
+    BTNode<String> s15 = new BTNode<String>("jennifer");
+    BTNode<String> s16 = new BTNode<String>("sheree");
+    BTNode<String> s17 = new BTNode<String>("michelle");
+    BTNode<String> s18 = new BTNode<String>("sam");
+    
+   
+    
+  }
+  
+}
